a0a6a919656e4380cb3fdf284d724950d61e759d
Nikola Philipp <nph@personizer.com>
2022-10-19 19:31:58 +0200

    Initial commit

---
+++ app/src/main/java/ba/nsoft/githubviewerexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package ba.nsoft.githubviewerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ba.nsoft.githubviewerexample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
